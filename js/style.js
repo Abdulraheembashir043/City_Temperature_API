dbf9633c822dd1cfda3740d793ae064180314804
Abdulraheem Bashir <abdulraheembashir043@gmail.com>
2021-03-19 11:06:28 +0100

    logic and interface update

--- js/style.js
+++ js/style.js
@@ -5,24 +5,22 @@ const display = document.querySelector('.display-result');
 const title = document.getElementById('result-title');
 const description = document.getElementById('description');
 const image = document.querySelector('img');
+const errMsg = document.getElementById('error');
 const kelvin = document.querySelector('#kelvin');
 const fahrenheit = document.querySelector('#fahrenheit');
 const celsius = document.querySelector('#celsius');
 
 city.focus();
-
+let input;
 btn.addEventListener('click', () => {
-  if(city.value) {
-    title.innerHTML = `${city.value.toUpperCase()} Temperature`;
-  }
-  fetchAPI(city.value);
+  input = city.value;
+  fetchAPI(input);
 })
 
 clear.addEventListener('click', () => {
-  display.style.display = 'none';
+  update();
   city.value = '';
-  city.focus();
-  btn.style.marginBottom = '23px';
+  errMsg.style.display = 'none';
 })
 
 function fetchAPI(cityName) {
@@ -30,13 +28,36 @@ function fetchAPI(cityName) {
   .then((response) => response.json())
   .then((data) => {
 
-    let val = data.main.temp;
+    if(data.cod === 200) {
 
-    description.innerHTML = data.weather[0].main;
-    image.src = `https://api.openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    
-    kelvin.innerHTML = Math.floor(val) + ` &degK`;
-    fahrenheit.innerHTML = Math.floor(9 / 5 * (val - 273) + 32) + ' &deg;F';
-    celsius.innerHTML = Math.floor(val - 273.15) + ' &deg;C';
+      if(cityName) {
+        title.innerHTML = `${cityName.toUpperCase()} Temperature`;
+      }
+
+      display.style.display = 'block';
+      errMsg.style.display = 'none';
+
+      let val = data.main.temp;
+
+      description.innerHTML = data.weather[0].main;
+      image.src = `https://api.openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      
+      kelvin.innerHTML = Math.floor(val) + ` &degK`;
+      fahrenheit.innerHTML = Math.floor(9 / 5 * (val - 273) + 32) + ' &deg;F';
+      celsius.innerHTML = Math.floor(val - 273.15) + ' &deg;C';
+    } else if(data.cod === "404") {
+      console.log('not correct');
+      update();
+      errMsg.style.display = 'block';
+    }
   })
 }
+
+function update() {
+  display.style.display = 'none';
+  city.focus();
+}
+
+window.addEventListener('load', function() {
+  city.value = '';
+})
